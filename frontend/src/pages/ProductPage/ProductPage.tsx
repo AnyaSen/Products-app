d1c18a22e4e42528a9a8f72b6c97bbfae220f8e3
AnyaSen <anyasenchikhina18@gmail.com>
2021-01-29 12:00:16 +0200

    Styled product page

--- frontend/src/pages/ProductPage/ProductPage.tsx
+++ frontend/src/pages/ProductPage/ProductPage.tsx
@@ -3,9 +3,13 @@ import { RouteComponentProps } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { IAppState } from "../../redux/store";
 import { ThunkDispatch } from "redux-thunk";
+import { fetchProductById } from "../../redux/actions";
+
+import Styles from "./ProductPage.module.scss";
+
 import LoadingPage from "../LoadingPage";
 import ErrorPage from "../ErrorPage";
-import { fetchProductById } from "../../redux/actions";
+import PricePerUnit from "../../components/shared/PricePerUnit";
 
 interface MatchParams {
   id: string;
@@ -28,10 +32,28 @@ export default function ProductPage({ match }: Props): ReactElement {
 
   if (isLoading) return <LoadingPage />;
   if (isError) return <ErrorPage />;
+  const { name, price, description, pricePerKg } = currentProduct;
+  const { priceEuros, priceCents, unit } = price;
 
   return (
-    <div>
-      <h1>{currentProduct.name}</h1>
+    <div className={Styles.ProductPage}>
+      <h1>Product information</h1>
+      <div className={Styles.ProductInfoAndPhoto}>
+        <img src={`/products/${id}/img`} />
+
+        <div className={Styles.ProductInfo}>
+          <h2>{name}</h2>
+          <p className={Styles.ProductPrice}>
+            {priceEuros}.{priceCents}/{unit}
+            <span>{pricePerKg}/kg</span>
+          </p>
+        </div>
+      </div>
+
+      <div className={Styles.ProductDescription}>
+        <h2>Description</h2>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
